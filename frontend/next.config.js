df772a2c50c6e56eb923692edc9d5f706fb45327
Dwinanda <dwinandam12@gmail.com>
2026-06-26 20:24:21 +0700

    fix: isolate frontend-backend comms on private network with unique hostname

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -90,7 +90,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://backend:5000/api/:path*'
+                destination: 'http://statify-backend:5000/api/:path*'
             }
         ];
     },
