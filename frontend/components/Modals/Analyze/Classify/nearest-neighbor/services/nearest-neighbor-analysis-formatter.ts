47b5fafb2a50f49c4fa8e3d17a8849959d303c5a
enayey <louistathaniaharahap14@gmail.com>
2026-09-10 10:23:14 +0700

    Revert "fix: update gitignore and untrack ignored files"
    This reverts commit ff6e133cbcb575bc752d4ec3e483e8c3f4caefc1, reversing
    changes made to a655909d2c6a60e3046e0e7b7a705312018cd57f.

--- frontend/components/Modals/Analyze/Classify/nearest-neighbor/services/nearest-neighbor-analysis-formatter.ts
+++ frontend/components/Modals/Analyze/Classify/nearest-neighbor/services/nearest-neighbor-analysis-formatter.ts
@@ -238,7 +238,7 @@ function buildConfusionMatrix(table: any): Table {
 
   return {
     key: "confusion_matrix",
-    title: "Classification Table",
+    title: "Classification Tablee",
     columnHeaders: [
       { header: "Partition", key: "partition" },
       { header: "Observed", key: "observed" },
